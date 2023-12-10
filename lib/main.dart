2845069dee37d56119ddd774ddd134495d5a9af9
zykuriku <104116273+zykuriku@users.noreply.github.com>
2023-12-11 01:54:38 +0530

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'choice_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
